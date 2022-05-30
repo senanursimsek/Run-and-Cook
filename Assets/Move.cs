fe5f19dcbb17913c7b5d72ac745dd3c9ee12ce40
Senanur <senanursimsekk@gmail.com>
2022-05-30 21:10:27 +0300

    leey

---
+++ Assets/Move.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+public float speed;
+public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    transform.position += transform.forward * Time.deltaTime * speed;    
+    }
+void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Jump")
+            anim.SetTrigger("Jump");
+        if(other.tag == "Fail")
+            anim.SetTrigger("Fail");
+        if(other.tag == "Walk")
+            anim.SetTrigger("Walk");
+    }
+}
